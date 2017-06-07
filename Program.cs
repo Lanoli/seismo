4bb8d761dcae5cc705f4981fe9cef65bfcf84338
bart moonen <mail@bartmoonen.be>
2017-06-07 22:17:23 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Seismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+
+            client.ConnectAsync("192.168.1.28",18000).Wait();
+
+
+            NetworkStream stream = client.GetStream();
+
+           var data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+
+        }
+    }
+}
